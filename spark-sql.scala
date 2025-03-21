f00079335e911f0e8adeb4ca23dfaeba46d83b2e
devbenweb3 <147643967+devbenweb3@users.noreply.github.com>
2025-03-21 02:52:13 +0000

    commit

--- spark-sql.scala
+++ spark-sql.scala
@@ -3,3 +3,7 @@ spark.sql("SHOW CATALOGS").show()
 spark.sql("SHOW DATABASES IN iceberg").show()
 
 spark.sql("SELECT * FROM iceberg.mydb.orders").show()
+
+spark.sql("""
+    INSERT INTO iceberg.mydb.orders VALUES (3, 'Product Spark 3', 300.0);
+""")
