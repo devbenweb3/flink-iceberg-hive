41a33c2327fd8178014291def18281cbff29150f
devbenweb3 <147643967+devbenweb3@users.noreply.github.com>
2025-03-21 07:16:23 +0000

    add spark py

---
+++ pyspark_etl/job_insert_order/insert_orders.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("insert_orders") \
+    .config("spark.sql.catalog.iceberg", "org.apache.iceberg.spark.SparkCatalog") \
+    .config("spark.sql.catalog.iceberg.type", "hive") \
+    .config("spark.sql.catalog.iceberg.uri", "thrift://hive-metastore:9083") \
+    .config("spark.sql.catalog.iceberg.warehouse", "s3://warehouse/") \
+    .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
+    .config("spark.hadoop.fs.s3a.access.key", "admin") \
+    .config("spark.hadoop.fs.s3a.secret.key", "password") \
+    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+    .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
+    .enableHiveSupport() \
+    .getOrCreate()
+
+df = spark.sql("SELECT * FROM iceberg.mydb.orders")
+df.show()
+
+# Dừng SparkSession
+spark.stop()
---
+++ pyspark_etl/job_insert_order/start.sh
@@ -0,0 +1,19 @@
+docker run --rm --name spark_job_insert_orders \
+  --network fih \
+  -v $(pwd)/insert_orders.py:/opt/spark/scripts/insert_orders.py \
+  -v $(pwd)/spark_jars:/opt/spark/jars \
+  bitnami/spark:latest \
+  /bin/sh -c "/opt/bitnami/spark/bin/spark-submit \
+    --master spark://spark-master:7077 \
+    --deploy-mode client \
+    --conf spark.sql.catalog.iceberg=org.apache.iceberg.spark.SparkCatalog \
+    --conf spark.sql.catalog.iceberg.type=hive \
+    --conf spark.sql.catalog.iceberg.uri=thrift://hive-metastore:9083 \
+    --conf spark.sql.catalog.iceberg.warehouse=s3://warehouse/ \
+    --conf spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions \
+    --conf spark.hadoop.fs.s3a.endpoint=http://minio:9000 \
+    --conf spark.hadoop.fs.s3a.access.key=admin \
+    --conf spark.hadoop.fs.s3a.secret.key=password \
+    --conf spark.hadoop.fs.s3a.path.style.access=true \
+    --jars /opt/spark/jars/iceberg-spark-runtime-3.5_2.12-1.8.1.jar \
+    /opt/spark/scripts/insert_orders.py"
