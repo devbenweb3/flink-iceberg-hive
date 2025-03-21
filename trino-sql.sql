9c9d4b582c0305c64f7f4b66d38b4338c34a0608
devbenweb3 <147643967+devbenweb3@users.noreply.github.com>
2025-03-21 02:32:56 +0000

    commit

--- trino-sql.sql
+++ trino-sql.sql
@@ -1,11 +1,22 @@
-SHOW SCHEMAS FROM hive;
-
 SHOW CATALOGS;
 
-SHOW TABLES FROM hive.mydb;
+SHOW SCHEMAS FROM iceberg;
 
-DESCRIBE hive.mydb.orders;
+CREATE SCHEMA iceberg.mydb WITH (location = 's3a://warehouse/mydb/' );
 
+SHOW TABLES FROM iceberg.mydb;
 
-select * FROM mydb.orders;
+CREATE TABLE iceberg.mydb.orders (
+    id BIGINT,
+    name VARCHAR,
+    amount DOUBLE
+)
+WITH (
+    format = 'parquet',
+    location = 's3a://warehouse/mydb/orders/'
+);
 
+INSERT INTO iceberg.mydb.orders VALUES (1, 'Product A', 100.0);
+INSERT INTO iceberg.mydb.orders VALUES (2, 'Product B', 200.0);
+
+SELECT * FROM iceberg.mydb.orders;
