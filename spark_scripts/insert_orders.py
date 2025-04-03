04b215a042d313aeeb42cadf160067c045e7ffc8
devbenweb3 <147643967+devbenweb3@users.noreply.github.com>
2025-04-03 05:43:38 +0000

    crud

--- spark_scripts/insert_orders.py
+++ spark_scripts/insert_orders.py
@@ -20,5 +20,22 @@ df.show()
 df1 = spark.sql("SELECT COUNT(1) FROM iceberg.mydb.orders")
 df1.show()
 
+spark.sql(" INSERT INTO iceberg.mydb.orders VALUES (3, 'Product 3', 300.0) ")
+spark.sql(" INSERT INTO iceberg.mydb.orders VALUES (4, 'Product 4', 400.0) ")
+spark.sql(" INSERT INTO iceberg.mydb.orders VALUES (5, 'Product 5', 500.0) ")
+
+df2 = spark.sql("SELECT * FROM iceberg.mydb.orders")
+df2.show()
+
+spark.sql(" UPDATE iceberg.mydb.orders SET amount = 301.0 WHERE ID = 3")
+
+df3 = spark.sql("SELECT * FROM iceberg.mydb.orders")
+df3.show()
+
+spark.sql(" DELETE FROM iceberg.mydb.orders WHERE ID = 4")
+
+df4 = spark.sql("SELECT * FROM iceberg.mydb.orders")
+df4.show()
+
 # Dừng SparkSession
 spark.stop()
